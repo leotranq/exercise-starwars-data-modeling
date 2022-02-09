d81dd9e7e39f1972f2774061a649e088cd7e0141
leotranq <leotranq@gmail.com>
2022-02-09 17:11:48 +0000

    update

--- src/models.py
+++ src/models.py
@@ -8,13 +8,22 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    username = Column(String(250),nullable=False)
+    firstName = Column(string (250),nullable=False)
+    lastName = Column(string(250),nullable=False)
+    email = Column(string(250),nullable=False)
 
+         def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
 class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
